334ae567871ceadb99933f1b14f511dc091dbde3
Brandon Rainey <brandonhrainey@gmail.com>
2022-04-23 02:29:26 -0400

    fixed desktop button active state

--- components/DesktopNav.js
+++ components/DesktopNav.js
@@ -1,21 +1,56 @@
 import React, { useState } from 'react'
 
 export default function DesktopNav() {
+  const [navItem, setNavItem] = useState()
 
-  const [isActive, setIsActive] = useState(false)
+  function handleClick(e) {
+    e.preventDefault()
+
+    if (e.target.innerText == 'Home') {
+      console.log('gwgww')
+      setNavItem('Home')
+    }
+    if (e.target.innerText == 'About') {
+      setNavItem('About')
+    }
+    if (e.target.innerText == 'Contact') {
+      setNavItem('Contact')
+    }
+    if (e.target.innerText == 'Blog') {
+      setNavItem('Blog')
+    }
+    if (e.target.innerText == 'Careers') {
+      setNavItem('Careers')
+    }
+  }
+
+
 
   return (
-    <div className='desktopNavContainer'>
-        <nav className='desktopNav'>
-            <a>Home</a>
-            <a>About</a>
-            <a>Contact</a>
-            <a>Blog</a>
-            <a>Careers</a>
-        </nav>
-        <button>
-            Request Invite
-        </button>
+    <div className="desktopNavContainer">
+      <nav className="desktopNav">
+        <a 
+        onClick={handleClick}
+        className={`${navItem == 'Home' ? 'navHome' : ''}`}
+        >Home</a>
+        <a 
+        onClick={handleClick}
+        className={`${navItem == 'About' ? 'navAbout' : ''}`}
+        >About</a>
+        <a 
+        onClick={handleClick}
+        className={`${navItem == 'Contact' ? 'navContact' : ''}`}
+        >Contact</a>
+        <a 
+        onClick={handleClick}
+        className={`${navItem == 'Blog' ? 'navBlog' : ''}`}
+        >Blog</a>
+        <a 
+        onClick={handleClick}
+        className={`${navItem == 'Careers' ? 'navCareers' : ''}`}
+        >Careers</a>
+      </nav>
+      <button>Request Invite</button>
     </div>
   )
 }
